9ed72c979dfca668e772f034577446d1d3900784
xylae <itsxylae@gmail.com>
2025-10-08 18:26:09 +0800

    Update lspconfig.lua

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,13 +1,18 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function()
-		local lspconfig = require("lspconfig")
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        capabilities.textDocument.completion.completionItem.snippetSupport = false
+		-- Disable snippet support if you don’t want snippet completions
+		capabilities.textDocument.completion.completionItem.snippetSupport = false
 
-		lspconfig.clangd.setup({
-            capabilities = capabilities,
-        })
+		-- Use the new vim.lsp.config API (Neovim 0.10+)
+		vim.lsp.config["clangd"] = {
+			cmd = { "clangd" },
+			capabilities = capabilities,
+		}
+
+		-- Start or attach clangd automatically to matching buffers
+		vim.lsp.start(vim.lsp.config["clangd"])
 	end,
 }
