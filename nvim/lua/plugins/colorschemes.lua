96eb441be8f28e0fe4ba80f94a1e83a0a1c86a11
xylae <itsxylae@gmail.com>
2025-10-18 03:21:02 +0800

    Update colorschemes.lua

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,19 +1,5 @@
 return {
     {
-        "slugbyte/lackluster.nvim",
-        lazy = false,
-        priority = 1000,
-        -- config = function()
-        --     local lackluster = require("lackluster")
-        --         lackluster.setup({
-        --         }
-        --     })
-        -- end,
-        -- init = function()
-        --     vim.cmd.colorscheme("lackluster")
-        -- end,
-    },
-    {
         "Verf/deepwhite.nvim",
         priority = 1000,
         init = function()
